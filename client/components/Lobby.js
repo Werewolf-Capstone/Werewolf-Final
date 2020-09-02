769f64be9bc294f1bbf4089ce7018c322ee88254
Jonathan Albert <JonathanAlbert0115@gmail.com>
2020-09-02 15:30:46 -0400

    Installed material UI

--- client/components/Lobby.js
+++ client/components/Lobby.js
@@ -1,39 +1,42 @@
 import React from 'react'
+import {TextField, Container, Button, Box} from '@material-ui/core'
 
 const Lobby = ({
   username,
   handleUsernameChange,
   roomName,
   handleRoomNameChange,
-  handleSubmit
+  handleSubmit,
 }) => {
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Enter a room</h2>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="field"
-          value={username}
-          onChange={handleUsernameChange}
-          required
-        />
-      </div>
+    <Container>
+      <form onSubmit={handleSubmit}>
+        <h2>Enter a room</h2>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="field"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
 
-      <div>
-        <label htmlFor="room">Room name:</label>
-        <input
-          type="text"
-          id="room"
-          value={roomName}
-          onChange={handleRoomNameChange}
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="room">Room name:</label>
+          <input
+            type="text"
+            id="room"
+            value={roomName}
+            onChange={handleRoomNameChange}
+            required
+          />
+        </div>
 
-      <button type="submit">Submit</button>
-    </form>
+        <button type="submit">Submit</button>
+      </form>
+    </Container>
   )
 }
 
