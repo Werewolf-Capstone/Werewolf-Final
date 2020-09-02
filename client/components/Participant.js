06fa57e46fdf18c4a282dc99a1e91c678b4ea87c
Aleks Mitrovic <mitrovic.aleksandar@outlook.com>
2020-09-01 20:33:57 -0400

    added additional conditionals to Participant.js component

--- client/components/Participant.js
+++ client/components/Participant.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable complexity */
 import React, {useState, useEffect, useRef} from 'react'
 
 import VideoAudio from './VideoAudio'
@@ -15,7 +17,7 @@ const Participant = ({
   localRole,
   werewolfChoice,
   didSeerHit,
-  gameStarted
+  gameStarted,
 }) => {
   let i
   let shouldWePlay = true
@@ -86,3 +88,88 @@ const Participant = ({
         </button>
       </div>
     )
+  } else if (night && checkWerewolf && !checkSeer && localRole === 'seer') {
+    //console.log.log("DURIONG THE NIGHT AND WEREWOLVES ARE DONE, SEER IS NOT DONE, AND WE ARE THE SEER")
+    shouldWePlay = true
+    i = (
+      <div className="participant">
+        <h3>
+          DURIONG THE NIGHT AND WEREWOLVES ARE DONE, SEER IS NOT DONE, AND WE
+          ARE THE SEER , role= {localRole}
+        </h3>
+        <h3>{participant.identity}</h3>
+
+        <button onClick={(e) => handleSeerCheckButton(participant.identity)}>
+          Check Role
+        </button>
+      </div>
+    )
+  } else if (
+    night &&
+    checkWerewolf &&
+    checkSeer &&
+    !checkMedic &&
+    localRole === 'medic'
+  ) {
+    //console.log.log("DURING THE NIGHT AND WEREWOLVES ARE DONE AND SEE IS DONE AND MEDIC IS NOT DONE AND WE ARE THE MEDIC")
+    shouldWePlay = true
+    i = (
+      <div className="participant">
+        <h3>
+          DURING THE NIGHT AND WEREWOLVES ARE DONE AND SEER IS DONE AND MEDIC IS
+          NOT DONE AND WE ARE THE MEDIC , role= {localRole}
+        </h3>
+        <h3>{participant.identity}</h3>
+
+        <button onClick={(e) => handleMedicSaveButton(participant.identity)}>
+          Save Person
+        </button>
+      </div>
+    )
+  } else if (!gameStarted) {
+    console.log('SEER IS STILL NOT SEEING GAME STARTED')
+    shouldWePlay = true
+    i = (
+      <div className="participant">
+        <h3>GAME NOT STARTED, role= {localRole}</h3>
+        <h3>{participant.identity}</h3>
+        <h3>You are not allowed to see this person during the night</h3>
+      </div>
+    )
+  } else {
+    //console.log.log("DURING THE NIGHT BUT WE ARE A VANILLA VILLAGER")
+
+    shouldWePlay = false
+    i = (
+      <div className="participant">
+        <h3>
+          222DURING THE NIGHT BUT WE ARE A VANILLA VILLAGER, OR DONE WITH OUR
+          TASK, role= {localRole}
+        </h3>
+        <h3>{participant.identity}</h3>
+        <h3>You are not allowed to see this person during the night</h3>
+      </div>
+    )
+  }
+  if (shouldWePlay) {
+    return (
+      <div>
+        {i}
+        {/* <video ref={videoRef} autoPlay={shouldWePlay} muted={true} />
+        <audio ref={audioRef} autoPlay={shouldWePlay} muted={true} /> */}
+        <VideoAudio participant={participant} />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        {i}
+        {/* <video ref={videoRef} autoPlay={shouldWePlay} muted={true} />
+        <audio ref={audioRef} autoPlay={shouldWePlay} muted={true} /> */}
+        <div>You are not allowed to see video at this time</div>
+      </div>
+    )
+  }
+}
+
+export default Participant
