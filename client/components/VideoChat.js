24725d66820b547b27862d98ff60d392da3b95c0
Jonathan Albert <JonathanAlbert0115@gmail.com>
2020-09-03 14:37:49 -0400

    Landing page now reverts to werewolf background when the user signs out.

--- client/components/VideoChat.js
+++ client/components/VideoChat.js
@@ -7,34 +7,36 @@ const VideoChat = () => {
   const [roomName, setRoomName] = useState('')
   const [token, setToken] = useState(null)
 
-  const handleUsernameChange = useCallback(event => {
+  const handleUsernameChange = useCallback((event) => {
     setUsername(event.target.value)
   }, [])
 
-  const handleRoomNameChange = useCallback(event => {
+  const handleRoomNameChange = useCallback((event) => {
     setRoomName(event.target.value)
   }, [])
 
   const handleSubmit = useCallback(
-    async event => {
+    async (event) => {
       event.preventDefault()
       const data = await fetch('/video/token', {
         method: 'POST',
         body: JSON.stringify({
           identity: username,
-          room: roomName
+          room: roomName,
         }),
         headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(res => res.json())
+          'Content-Type': 'application/json',
+        },
+      }).then((res) => res.json())
       setToken(data.token)
     },
     [roomName, username]
   )
 
-  const handleLogout = useCallback(event => {
+  const handleLogout = useCallback((event) => {
     setToken(null)
+    document.getElementById('background').classList.add('lobby')
+    document.getElementById('background').classList.remove('day')
   }, [])
 
   let render
