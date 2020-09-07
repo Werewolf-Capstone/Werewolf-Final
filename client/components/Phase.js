710e542be531f009102dd76f1c682216287db1d0
Michael Pesek <pesekm@dupage.edu>
2020-09-06 22:27:13 -0500

    removed bad typo

--- client/components/Phase.js
+++ client/components/Phase.js
@@ -3,7 +3,7 @@ import React, {useEffect} from 'react'
 import Backdrop from '@material-ui/core/Backdrop'
 import {makeStyles} from '@material-ui/core/styles'
 import {Container, Box, Fade} from '@material-ui/core'
-import ImageCom from './ImageCom'
+
 import Zoom from '@material-ui/core/Zoom'
 
 const useStyles = makeStyles((theme) => ({
